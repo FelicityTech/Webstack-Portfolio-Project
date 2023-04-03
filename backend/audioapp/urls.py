a66410ec5c9e3320065c3bd3aba8aed8a02ddc77
Prideland-Okoi <prideland.okoi@gmail.com>
2023-04-03 16:41:43 +0100

    requirement text added

--- backend/audioapp/urls.py
+++ backend/audioapp/urls.py
@@ -4,7 +4,7 @@ from .import views
 app_name = 'audioapp'
 
 urlpatterns = [
-    path("", views.index, name = 'home'),
+    path("", views.home, name = 'home'),
     path('audio-create', views.audio_create, name = 'audio-create'),
     path('audio-details', views.audio_details, name ='audio-details'),
     path('audio-delete/<int:id>', views.audio_delete, name ='audio-delete'),
