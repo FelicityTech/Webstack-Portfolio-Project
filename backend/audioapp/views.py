049ec99e6aa39a3cf9a0d65e9cce8f1d0a51d4eb
Prideland-Okoi <prideland.okoi@gmail.com>
2023-04-04 02:50:18 +0000

    phase 1 done. Tested okay

--- backend/audioapp/views.py
+++ backend/audioapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from gTTS.templatetags.gTTS import say
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import TextAudio
 from .forms import TextAudioForm
@@ -14,25 +15,17 @@ def audio_create(request):
     if request.method == 'POST':
         text_form = TextAudioForm(request.POST)
         if text_form.is_valid():
-            lang = request.POST.get('lang')
-            text = request.POST.get('text')
-            audio_obj = say(language = lang, text = text)
             text_form.save()
-            text_form = TextAudioForm()
-            context = {'audio_obj':audio_obj,
-            'text_form':text_form,
-            }
-            return render(request, 'audioapp/audio_details.html',context)
-        else:
-            return redirect('audioapp:audio-create')
-    context = {}
-    contex['text_form'] = TextAudioForm()
+            return redirect('audioapp:audio-details')
+    else:
+        context = {}
+        context['text_form'] = TextAudioForm()
     return render(request, 'audioapp/audio_create.html', context)
 
 def audio_details(request):
     text_audio = TextAudio.objects.all()
     context = {}
-    contex['text_audio'] = TextAudio.objects.all()
+    context['text_audio'] = TextAudio.objects.all()
     return render(request, 'audioapp/audio_details.html', context)
 
 def audio_delete(request, id):
